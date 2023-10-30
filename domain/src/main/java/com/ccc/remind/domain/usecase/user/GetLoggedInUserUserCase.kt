3b3ce4363175d73076254ae485c7b12c520173c6
moonggae <egchoi.dev@gmail.com>
2023-10-30 18:45:29 +0900

    :bug: fix a get logged in user process

--- domain/src/main/java/com/ccc/remind/domain/usecase/user/GetLoggedInUserUserCase.kt
+++ domain/src/main/java/com/ccc/remind/domain/usecase/user/GetLoggedInUserUserCase.kt
@@ -3,9 +3,30 @@ package com.ccc.remind.domain.usecase.user
 import com.ccc.remind.domain.entity.user.User
 import com.ccc.remind.domain.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetLoggedInUserUserCase @Inject constructor(private val userRepository: UserRepository) {
-    suspend operator fun invoke() : Flow<User?> = userRepository.getLoggedInUser()
+    private var loggedInUser: User? = null
+    suspend operator fun invoke() : Flow<User?> = flow {
+        userRepository.getLoggedInUser().collect { localUser ->
+            loggedInUser = localUser
+        }
 
+        loggedInUser?.let { user ->
+            userRepository.getUserProfile().collect { profile ->
+                loggedInUser = user.copy(
+                    displayName = profile.displayName,
+                    inviteCode = profile.inviteCode,
+                    profileImage = profile.profileImage
+                )
+            }
+        }
+
+        loggedInUser?.let { user ->
+            userRepository.replaceLoggedInUser(user)
+        }
+
+        emit(loggedInUser)
+    }
 }
