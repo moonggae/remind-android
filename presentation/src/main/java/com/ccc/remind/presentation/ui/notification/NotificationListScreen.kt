c7ba63ac8582c8a95b165ce93fd29f534bd0c6ea
moonggae <egchoi.dev@gmail.com>
2023-11-03 21:09:31 +0900

    :bug: read all notifications when current page is notification list page and get new notification

--- presentation/src/main/java/com/ccc/remind/presentation/ui/notification/NotificationListScreen.kt
+++ presentation/src/main/java/com/ccc/remind/presentation/ui/notification/NotificationListScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
@@ -73,6 +74,10 @@ fun NotificationListScreen(
 ) {
     val scope = rememberCoroutineScope()
     val uiState by viewModel.uiState.collectAsState()
+
+    LaunchedEffect(uiState.notifications) {
+        viewModel.updateNotificationsReadAll()
+    }
 
     BasicScreen(
         viewModel = viewModel,
