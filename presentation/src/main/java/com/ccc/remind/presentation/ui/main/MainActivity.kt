50f52e2df12101cddb15e0fa9e238fc88bac8de6
moonggae <egchoi.dev@gmail.com>
2023-12-07 15:22:02 +0900

    :chart_with_upwards_trend: add screen view analytics

--- presentation/src/main/java/com/ccc/remind/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/ccc/remind/presentation/ui/main/MainActivity.kt
@@ -11,14 +11,18 @@ import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.ccc.remind.presentation.di.network.InterceptorOkHttpClient
+import com.ccc.remind.presentation.navigation.moveToNotificationRoute
 import com.ccc.remind.presentation.ui.App
 import com.ccc.remind.presentation.ui.SharedViewModel
-import com.ccc.remind.presentation.navigation.moveToNotificationRoute
 import com.ccc.remind.presentation.ui.onboard.login.LoginActivity
 import com.ccc.remind.presentation.util.Constants.NOTIFICATION_INTENT_EXTRA_TARGET_ID
 import com.ccc.remind.presentation.util.Constants.NOTIFICATION_INTENT_EXTRA_TYPE
 import com.ccc.remind.presentation.util.initCoil
 import com.ccc.remind.presentation.util.notification.NotificationUtil
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.analytics.ktx.logEvent
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
@@ -32,6 +36,7 @@ class MainActivity : ComponentActivity() {
     val sharedViewModel: SharedViewModel by viewModels()
     private val mainViewModel: MainViewModel by viewModels()
     private lateinit var navController: NavController
+    private val firebaseAnalytics = Firebase.analytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,6 +52,11 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             navController = rememberNavController()
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                firebaseAnalytics.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+                    param(FirebaseAnalytics.Param.SCREEN_NAME, destination.route ?: "unknown")
+                }
+            }
 
             App(
                 navController = navController,
